136b76883922e4f6f96564a27a8b92e8f44ba1a2
0xmarf <116585476+0xmarf@users.noreply.github.com>
2023-02-10 16:41:51 -0500

    Add files via upload

--- client/index.js
+++ client/index.js
@@ -1,14 +1,26 @@
 const axios = require('axios');
 const niceList = require('../utils/niceList.json');
 const MerkleTree = require('../utils/MerkleTree');
+const readlineSync = require('readline-sync');
+
 
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // Prompt the user to enter their name
+  const name = readlineSync.prompt("Enter your name:");
 
+  const merkleTree = new MerkleTree(niceList);
+  const root = merkleTree.getRoot();
+
+  // Get the index of the name in the nice list
+  const index = niceList.indexOf(name);
+
+  // Make the request to the server, including the name as a body parameter
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name,
+    root,
+    index,
   });
 
   console.log({ gift });
--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const verifyProof = require('../utils/verifyProof');
+const niceList = require('../utils/niceList.json');
+const MerkleTree = require('../utils/MerkleTree');
 
 const port = 1225;
 
@@ -8,22 +10,29 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT = '0f375892c33c3c630693ef103efe846d37c66d1d13050703538476caaeedc99f';
+const merkleTree = new MerkleTree(niceList);
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
   const body = req.body;
-
   // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
-    res.send("You got a toy robot!");
-  }
-  else {
-    res.send("You are not on the list :(");
-  }
-});
+  // Check if the name is in the list
+  const name = body.name;
+  if (niceList.includes(name)) {
+  // Prove that the name is in the list
+    const index = niceList.indexOf(name);
+    const proof = merkleTree.getProof(index);
+    const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}!`);
-});
+    if (isInTheList) {
+      res.send("You got a toy robot!");
+    } else {
+      res.send("You are not on the list :(");
+    }
+  });
+  
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}!`);
+  });
+
